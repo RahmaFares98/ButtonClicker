5781553537694f07ebe6889a5e146305fefe5cc6
unknown <rahmafaris981@gmail.com>
2024-05-17 19:37:31 +0300

    first commit

---
+++ button.clicker.js
@@ -0,0 +1,20 @@
+/*
+Desc: This function will do...
+Developer name: 
+Email:
+Input:
+output:
+*/
+function login(element)
+{
+    element.innerText = "SignOut";
+}
+
+function hide(element) {
+    element.remove();
+}
+
+function clicked(element) {
+    alert("Ninja Was Liked" );
+}
+
